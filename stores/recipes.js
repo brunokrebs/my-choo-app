8953c8fb6b6c6a35a6653df4c08ba4c77a76dceb
Bruno Krebs <bruno.krebs@auth0.com>
2018-08-20 20:29:43 -0300

    adding event and event listener to load a single recipe;

--- stores/recipes.js
+++ stores/recipes.js
@@ -4,16 +4,30 @@ module.exports = store
 
 function store (state, emitter) {
   state.recipes = []
+  state.recipe = {}
 
   state.events.LOAD_RECIPES = 'loadRecipes'
+  state.events.LOAD_RECIPE = 'loadRecipe'
 
-  emitter.on(state.events.LOAD_RECIPES, function () {
+  emitter.on(state.events.DOMCONTENTLOADED, function () {
     axios.get('http://localhost:8081/').then(function (response) {
       state.error = null
       state.recipes = response.data
       emitter.emit(state.events.RENDER)
     }).catch(function (err) {
       state.error = 'Unable to load recipes'
+      console.log(err)
+      emitter.emit(state.events.RENDER)
+    })
+  })
+
+  emitter.on(state.events.LOAD_RECIPE, function (id) {
+    axios.get('http://localhost:8081/' + id).then(function (response) {
+      state.error = null
+      state.recipe = response.data
+      emitter.emit(state.events.RENDER)
+    }).catch(function (err) {
+      state.error = 'Unable to load recipe with id: ' + id
       console.log(err)
       emitter.emit(state.events.RENDER)
     })
