faefdce161c8bee4fab77bc833c32cbbca895521
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2018-08-23 16:34:28 +0100

    Added new view for authenticated users and fixed string-to-conversion

---
+++ views/user.js
@@ -0,0 +1,22 @@
+var html = require('choo/html')
+var Header = require('../components/header')
+var AddRecipes = require('../components/addRecipe')
+
+var TITLE = 'My Choo App'
+
+module.exports = view
+
+function view (state, emit) {
+    if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
+
+    emit(state.events.AUTHENTICATE)
+
+    return html`
+    <body>
+    ${state.cache(Header, 'header').render()}            
+    <div class="container-fluid">
+        ${state.cache(AddRecipes, 'addRecipe').render()}
+    </div>
+    </body>
+    `
+}
