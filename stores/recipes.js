beef7cf509761f52a59ba06250add6c27daf69fb
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2018-08-31 16:07:16 +0100

    remove unwanted code

--- stores/recipes.js
+++ stores/recipes.js
@@ -26,18 +26,6 @@ function store (state, emitter) {
     emitter.emit(state.events.LOAD_RECIPES)
   })
 
-  // emitter.on(state.events.PUSHSTATE, '/', function() {
-  //   axios.get('http://localhost:8081/').then(function (response) {
-  //     state.error = null
-  //     state.recipes = response.data
-  //     emitter.emit(state.events.RENDER)
-  //   }).catch(function (err) {
-  //     state.error = 'Unable to load recipes'
-  //     console.log(err)
-  //     emitter.emit(state.events.RENDER)
-  //   })
-  // })
-
   emitter.on(state.events.LOAD_RECIPE, function (id) {
     axios.get('http://localhost:8081/' + id).then(function (response) {
       state.error = null
